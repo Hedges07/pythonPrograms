c6f8b650b7787695d1a30189f8bd6a6995e5fc31
Tyler Johnston <57685547+Hedges07@users.noreply.github.com>
2022-06-10 19:57:17 -0400

    Update Assignment1Q3.py

--- Assignment1Q3.py
+++ Assignment1Q3.py
@@ -1,6 +1,5 @@
 #Tyler Johnston
 a = int(input("Enter your age: "))
 r = int(input("Enter your resting heart rate: "))
-
 trainingRate = (.7*(220-a) +(.3*r))
 print ("Your Training Rate(beats/min) is : ", trainingRate)
