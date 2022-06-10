01df160becdc0ba8b447d233cfe71dc437322259
Tyler Johnston <57685547+Hedges07@users.noreply.github.com>
2022-06-10 19:59:09 -0400

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -1,11 +1,6 @@
-#Tyler Hedges-Johnston
-#25402346   
-#Programming for beginners Lab 3
-
 pi = 3.142
 radius = int(input("Enter the radius of the circle: "))
 area = pi*radius*radius
 volume = (4*(pi*radius*radius*radius))/3
 print("The area of the circle is ", area)
-print("The volume of the circle is ", volume) 
-
+print("The volume of the circle is ", volume)
