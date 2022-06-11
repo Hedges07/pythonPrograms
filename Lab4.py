45406a8e036763ff65746de32efd9a31254e48f9
Tyler Johnston <57685547+Hedges07@users.noreply.github.com>
2022-06-10 20:00:31 -0400

    Update Lab4.py

--- Lab4.py
+++ Lab4.py
@@ -1,3 +1,7 @@
+#Tyler Hedges-Johnston
+#25402346   
+#Programming for beginners Lab 4
+
 mealPrice = float(input("Enter the amount of the meal: "))
 tip = round((mealPrice * .15), 2)
 tax = round((mealPrice * .13) ,2) 
@@ -5,4 +9,4 @@ mealPrice = round(mealPrice+tax+tip,2)
 
 print("Your total price is ", mealPrice)
 print("Your tip was ", tip)
-print("Your tax was ", tax)  
+print("Your tax was ", tax) 
